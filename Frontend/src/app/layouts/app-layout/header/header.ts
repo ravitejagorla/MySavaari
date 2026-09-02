13013abda1b09d2b2ffcfc3a1821fbcc0fe09018
ravitejagorla <gorla.ravitejayadav@gmail.com>
2026-09-02 17:31:20 +0530

    taking backup

--- Frontend/src/app/layouts/app-layout/header/header.ts
+++ Frontend/src/app/layouts/app-layout/header/header.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ThemeToggleButtonComponent } from '../../../shared/components/common/theme-toggle/theme-toggle-button.component';
 import { AuthService } from '../../../core/services/auth.service';
+import { AppConfigService } from '../../../core/services/app-config.service';
+import { MyComponent } from "../../../shared/components/common/Full-Screen/full-screen";
 
 @Component({
-  imports: [],
   selector: 'ras-header',
-  styleUrl: './header.css',
+  standalone: true,
+  imports: [ThemeToggleButtonComponent, MyComponent],
   templateUrl: './header.html',
+  styleUrl: './header.css',
 })
 export class Header {
-  constructor(private authService: AuthService) { }
-  
-    logout(): void {
-      this.authService.logout();
-    }
+
+  readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+  constructor(protected readonly appConfig: AppConfigService,){}
+
+  isUserMenuOpen = false;
+
+  toggleUserMenu(): void {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  closeUserMenu(): void {
+    this.isUserMenuOpen = false;
+  }
+
+  logout(): void {
+    this.closeUserMenu();
+    this.authService.logout();
+  }
 }
